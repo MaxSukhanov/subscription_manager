8486ea7a0d47552787c17b75d806553435864ae4
Max Sukhanov <fubar00@yandex.ru>
2020-02-11 09:04:51 +0300

    create_payment_form

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,20 +1,27 @@
 require 'create_subscription/create_subscription.rb'
 class SubscriptionsController < ApplicationController
-  #before_action :find_plan, only: [:create]
+  before_action :find_plan, only: [:create]
+  skip_before_action :verify_authenticity_token
+  
   def new
   end
 
   def create
-    result = CreateStripeSubscription.call(
-      customer: session[:user_id]
+    CreateStripeSubscription.call(
+      customer: session[:user_id],
+      plan: plan,
+      subscription: subscription_params
     )
-    if result.success?
-      redirect_to '/products'
-    end   
+    redirect_to '/products'
   end
+
 
   private
     def find_plan
-      Plan.find_by(product_id: params[:product_id])
+      @plan = Plan.find_by(product_id: params[:product_id])
+    end
+
+    def subscription_params
+      params.permit(:stripeToken)
     end
 end
