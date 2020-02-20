4d9089ae674cf7e3498b1b89c18c6cb102a17d04
Max Sukhanov <fubar00@yandex.ru>
2020-02-20 23:30:04 +0300

    Revert "Revert "add_changes_from_master_branch""
    This reverts commit 17b342cf6549f13dafe8e2272a2f1bd08535c64d.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   resources :products
   resources :plans
   resources :subscriptions
   resources :customers
+=======
+
+  resources :products,
+            :plans,
+            :subscriptions,
+            :customers
+>>>>>>> 38da3e0d0e2d0fe9e3befeffe3fd4dc6be73dcdc
 end
