1d37c16dfe4a97c8ff6d6a5065b5625dd00acd10
Max Sukhanov <fubar00@yandex.ru>
2020-02-20 18:48:32 +0300

    change_routes_resources_formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :products,
-            :plans,
-            :subscriptions,
-            :customers
+  resources :products
+  resources :plans
+  resources :subscriptions
+  resources :customers
 end
