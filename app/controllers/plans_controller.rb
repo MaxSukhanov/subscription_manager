0309828edcfd03ba197d5467f1486e0fdced5c86
Max Sukhanov <fubar00@yandex.ru>
2020-03-12 11:35:12 +0300

    modify_plans_views_and_controller

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,20 +1,31 @@
 require 'create_plan/create_plan.rb'
 class PlansController < ApplicationController
- 
+
+  def index
+    @plans = Plan.all
+  end
+
+  def new
+    @plan = Plan.new
+  end
+
   def create
     result = CreateStripePlan.call(
-        product: product
+        plan: plan_params
       )
     if result.success?
-      redirect_to products_path
+      redirect_to admin_plans_path
     else
       render action: 'new'
     end
   end
 
-  private
+  def destroy
+    @plan.destroy
+    redirect_to admin_plans_path, notice: "Plan was deleted"
+  end
 
-    def product
-      Product.find(params[:product_id])
-    end
+  private
+    def plan_params
+      params.require(:plan).permit(:product, :interval, :amount)
 end
